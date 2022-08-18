a9fb9eca4de6e8cabd035c01c7f4829156586e08
Dylan Masschelein <dylanmasschelein@gmail.com>
2022-08-17 17:18:42 -0700

    Fixed build err, styling and responsiveness, added action button functionalities, reorganized, setup some initial tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-import type { GetServerSideProps, NextPage } from 'next';
+import type { GetServerSideProps } from 'next';
 import { FC } from 'react';
 import { getActivityArray } from '../src/api';
 import { defaultQueryArr } from '../src/static/global_vars';
-import { IActivityResponse } from '../src/types';
+import { IActivity } from '../src/types';
 import Home from '../src/components/home';
 
 interface IProps {
-	defaultActivityList: IActivityResponse[];
+	defaultActivityList: IActivity[];
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-	const defaultActivityList: IActivityResponse[] = await getActivityArray(defaultQueryArr);
+	const defaultActivityList: IActivity[] = await getActivityArray(defaultQueryArr);
 	return {
 		props: { defaultActivityList }
 	};
